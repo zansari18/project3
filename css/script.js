df2dc7f79dda113dd47abfb1f6734e5ff56b3d06
zansari18 <143058076+zansari18@users.noreply.github.com>
2023-11-21 21:53:48 -0600

    js configure
    the maps aren't loading, and I added a nav bar and even styled it, but the map still doesnt load.

--- css/script.js
+++ css/script.js
@@ -1,7 +1,8 @@
 var map;
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat:31.5883, lng: 74.3105},
-		zoom: 8
+		center: {lat: 31.5883, lng: 74.3105},
+		zoom: 19
 	});
 }
+initMap();
