ad297ef95c25b1bc7e521c1c551f49cb352d226c
zansari18 <143058076+zansari18@users.noreply.github.com>
2023-11-21 22:22:47 -0600

    rewrote my html, css, and java
    I used the google API thing, I don't think its going to work but it is worth a shot

--- css/script.js
+++ css/script.js
@@ -1,8 +1,15 @@
-var map;
 function initMap() {
-	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 31.5883, lng: 74.3105},
-		zoom: 19
-	});
+  const myLatLng = { lat: 31.5883, lng: 74.3105 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 18,
+    center: myLatLng,
+  });
+
+  new google.maps.Marker({
+    position: myLatLng,
+    map,
+    title: "Badshahi Mosque",
+  });
 }
-initMap();
+
+window.initMap = initMap;
