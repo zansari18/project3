de96165407a44f3575dbdfeab4da8a1eab21c2af
zansari18 <143058076+zansari18@users.noreply.github.com>
2023-12-04 11:58:41 -0600

    had to rewrite the javascript
    using const

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
-function myMap() {
-	var mapProp = {
-		center:new google.maps.LatLng(51.508742,-0.120850),
-		zoom:5,
-	};
-	var map = new google.maps.Map(document.getElementById("map"),mapProp);
-}
+async function initMap() {
+  
+  const position = {lat: 51.508742,lng: -0.120850};
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+  map = new Map(document.getElementById("map"), {
+    zoom: 4,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+  initMap();
+
